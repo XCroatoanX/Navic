3c7ff01f6af6627ff1e4f2f34fe1a4b3b840daf8
paige <paigely@tuta.io>
2026-01-25 01:56:14 -0600

    chore: bump version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -93,7 +93,7 @@ android {
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
 		versionCode = 3
-		versionName = "1.0.0-alpha10"
+		versionName = "1.0.0-alpha11"
 	}
 
 	signingConfigs {
