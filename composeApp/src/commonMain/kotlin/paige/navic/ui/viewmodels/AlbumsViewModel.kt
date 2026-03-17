a41d69f0520c6810f4a4b72cd4315906c23a4a01
Heropowwa <fittiziogiovanni@gmail.com>
2026-03-17 21:33:33 +0100

    fix: unused import

--- composeApp/src/commonMain/kotlin/paige/navic/ui/viewmodels/AlbumsViewModel.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/ui/viewmodels/AlbumsViewModel.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import paige.navic.data.models.Settings
 import paige.navic.data.repositories.AlbumsRepository
 import paige.navic.data.session.SessionManager
 import paige.navic.utils.UiState
