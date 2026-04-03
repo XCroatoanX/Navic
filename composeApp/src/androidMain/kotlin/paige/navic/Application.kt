e8f75094d7ec0a9429cf395208d2933d82de04b1
paige <paigely@tuta.io>
2026-04-03 20:38:18 +0200

    chore: rename application class

--- composeApp/src/androidMain/kotlin/paige/navic/Application.kt
+++ composeApp/src/androidMain/kotlin/paige/navic/Application.kt
@@ -1,16 +1,15 @@
 package paige.navic
 
-import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import paige.navic.di.initKoin
 
-class NavicApplication : Application() {
+class Application : android.app.Application() {
 	override fun onCreate() {
 		super.onCreate()
 
 		initKoin {
-			androidContext(this@NavicApplication)
+			androidContext(this@Application)
 			androidLogger()
 		}
 	}
