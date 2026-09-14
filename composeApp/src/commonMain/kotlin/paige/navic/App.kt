786b81533694166f4d05dc4d55cbebed3bd0cebb
XCroatoanX <stasyukartem2@gmail.com>
2026-09-15 00:07:34 +0200

    cleanup

--- composeApp/src/commonMain/kotlin/paige/navic/App.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/App.kt
@@ -35,7 +35,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
