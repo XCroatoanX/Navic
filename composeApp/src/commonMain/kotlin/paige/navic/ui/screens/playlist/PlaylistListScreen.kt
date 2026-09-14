dc75d672724af227e15f047beb32e1b41689cf5c
XCroatoanX <stasyukartem2@gmail.com>
2026-09-15 00:06:41 +0200

    Fix FAB being under bottombar and miniplayer

--- composeApp/src/commonMain/kotlin/paige/navic/ui/screens/playlist/PlaylistListScreen.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/ui/screens/playlist/PlaylistListScreen.kt
@@ -42,6 +42,7 @@ import navic.composeapp.generated.resources.title_playlists
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
 import org.koin.compose.viewmodel.koinViewModel
+import paige.navic.di.LocalBottomBarPadding
 import paige.navic.di.LocalBottomBarScrollManager
 import paige.navic.domain.manager.PreferenceManager
 import paige.navic.domain.models.DomainSongCollection
@@ -157,7 +158,9 @@ fun PlaylistListScreen(
 				}
 			) { notScrolled ->
 				if (notScrolled) {
+					val bottomBarPadding = LocalBottomBarPadding.current
 					MediumFloatingActionButton(
+						modifier = Modifier.padding(bottom = bottomBarPadding),
 						shape = MaterialTheme.shapes.large,
 						containerColor = MaterialTheme.colorScheme.primary,
 						onClick = {
--- composeApp/src/commonMain/kotlin/paige/navic/ui/screens/radio/RadioListScreen.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/ui/screens/radio/RadioListScreen.kt
@@ -38,6 +38,7 @@ import navic.composeapp.generated.resources.title_radios
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
 import org.koin.compose.viewmodel.koinViewModel
+import paige.navic.di.LocalBottomBarPadding
 import paige.navic.di.LocalBottomBarScrollManager
 import paige.navic.domain.manager.PreferenceManager
 import paige.navic.domain.models.settings.BottomBarCollapseMode
@@ -113,7 +114,9 @@ fun RadioListScreen(
 				}
 			) { notScrolled ->
 				if (notScrolled) {
+					val bottomBarPadding = LocalBottomBarPadding.current
 					MediumFloatingActionButton(
+						modifier = Modifier.padding(bottom = bottomBarPadding),
 						shape = MaterialTheme.shapes.large,
 						containerColor = MaterialTheme.colorScheme.primary,
 						onClick = {
