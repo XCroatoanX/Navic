58beb5b1a6087a07c50143a7299968901ce0257f
XCroatoanX <stasyukartem2@gmail.com>
2026-09-15 00:22:40 +0200

    Fix bottom bar prevent being shown on settings page

--- composeApp/src/commonMain/kotlin/paige/navic/App.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/App.kt
@@ -184,7 +184,8 @@ fun App() {
 				Scaffold(
 					modifier = Modifier.nestedScroll(scrollManager.connection),
 					bottomBar = {
-						if (isLoggedIn) {
+						val currentScreen = backStack.lastOrNull()
+						if (isLoggedIn && currentScreen !is Screen.Settings) {
 							RootBottomBar(scrolled = scrollManager.isTriggered)
 						}
 					},
